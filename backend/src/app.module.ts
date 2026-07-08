dbbc13ad6131765fcfad965f449c7406eb757bed
UCWSANGAM <ucwcrm@gmail.com>
2026-07-08 15:13:24 +0530

    Add files via upload

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { InvestorsModule } from './investors/investors.module';
+import { ImportModule } from './import/import.module';
+import { HealthController } from './health.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 100 }]), // global default rate limit
+    PrismaModule,
+    AuthModule,
+    InvestorsModule,
+    ImportModule,
+  ],
+  controllers: [HealthController],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
+})
+export class AppModule {}

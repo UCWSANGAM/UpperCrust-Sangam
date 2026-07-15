5dac96c559e622e6f3289f1b3758f22260196da0
UCWSANGAM <ucwcrm@gmail.com>
2026-07-15 15:30:56 +0530

    ok

--- backend/src/tasks/dto/update-task.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsOptional } from 'class-validator';
-import { TaskStatus } from '@prisma/client';
-
-export class UpdateTaskDto {
-  @IsOptional()
-  @IsEnum(TaskStatus)
-  status?: TaskStatus;
-}
